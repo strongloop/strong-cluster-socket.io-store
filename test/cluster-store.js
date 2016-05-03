59689307f43b382af5c46f14c9feff79bc0a9df2
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 16:35:21 -0700

    update/insert copyright notices

--- test/cluster-store.js
+++ test/cluster-store.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013,2015. All Rights Reserved.
+// Node module: strong-cluster-socket.io-store
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var cluster = require('cluster');
 var expect = require('chai').expect;
 var ioServer = require('socket.io');
